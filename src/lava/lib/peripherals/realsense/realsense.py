8ba0bafd147cc9629c26c13006d003ced8f567c8
gkarray <ghassen.karray@intel.com>
2023-09-20 17:04:31 +0200

    adding tests

--- src/lava/lib/peripherals/realsense/realsense.py
+++ src/lava/lib/peripherals/realsense/realsense.py
@@ -3,6 +3,7 @@
 # See: https://spdx.org/licenses/
 import numpy as np
 import os
+os.environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"
 import cv2
 import typing as ty
 from pathlib import Path
@@ -13,7 +14,6 @@ try:
 except ImportError:
     print("Need `pyrealsense2` library installed.", file=sys.stderr)
     exit(1)
-
 from lava.magma.core.process.process import AbstractProcess
 from lava.magma.core.process.ports.ports import OutPort
 from lava.magma.core.sync.protocols.loihi_protocol import LoihiProtocol
@@ -25,13 +25,13 @@ from lava.magma.core.model.py.model import PyLoihiProcessModel
 
 
 class Realsense(AbstractProcess):
-    """Process that reads RGB+Depth frames, either from a Realsense camera
+    """Process that reads BGR+Depth frames, either from a Realsense camera
     directly, or from recorded PNG+EXR files.
 
     If directory_path is not given, the Process will read and send RGB and
     Depth frames from a Realsense camera.
 
-    If directory_path is given, the Process will read and send RGB and Depth
+    If directory_path is given, the Process will read and send BGR and Depth
     frames from PNG and EXR files contained in the directory which the path
     points to.
     In this case, png_prefix and exr_prefix have to be provided to inform the
@@ -39,8 +39,8 @@ class Realsense(AbstractProcess):
 
     Parameters
     ----------
-    align_depth_to_rgb: bool, optional
-        Boolean flag controlling whether or not to align Depth frames to RGB
+    align_depth_to_bgr: bool, optional
+        Boolean flag controlling whether or not to align Depth frames to BGR
         frames.
     directory_path: str, optional
         Path to directory that contains the images PNG+EXR files.
@@ -50,13 +50,14 @@ class Realsense(AbstractProcess):
     exr_prefix: str, optional
         Prefix of EXR files (recording of Depth frames).
     """
+
     def __init__(self,
-                 align_depth_to_rgb: bool = True,
+                 align_depth_to_bgr: ty.Optional[bool] = True,
                  directory_path: ty.Optional[str] = None,
-                 png_prefix: ty.Optional[str] = None,
-                 exr_prefix: ty.Optional[str] = None) -> None:
+                 png_prefix: ty.Optional[str] = "",
+                 exr_prefix: ty.Optional[str] = "") -> None:
         super().__init__(
-            align_depth_to_rgb=align_depth_to_rgb,
+            align_depth_to_bgr=align_depth_to_bgr,
             directory_path=directory_path,
             png_prefix=png_prefix,
             exr_prefix=exr_prefix
@@ -70,84 +71,85 @@ class Realsense(AbstractProcess):
 
             profile = pipeline.start(config)
             device = profile.get_device()
-            rgb_sensor = device.first_color_sensor()
+            bgr_sensor = device.first_color_sensor()
             depth_sensor = device.first_depth_sensor()
 
-            if rgb_sensor is None:
-                raise ValueError("No RGB sensor was found.")
+            if bgr_sensor is None:
+                raise ValueError("No BGR sensor was found.")
             if depth_sensor is None:
                 raise ValueError("No Depth sensor was found.")
 
-            rgb_stream = \
+            bgr_stream = \
                 profile.get_stream(rs.stream.color).as_video_stream_profile()
             depth_stream = \
                 profile.get_stream(rs.stream.depth).as_video_stream_profile()
 
-            self.rgb_2d_shape = (rgb_stream.height(), rgb_stream.width())
-            self.rgb_3d_shape = self.rgb_2d_shape + (3,)
+            self.bgr_2d_shape = (bgr_stream.height(), bgr_stream.width())
+            self.bgr_3d_shape = self.bgr_2d_shape + (3,)
             self.depth_2d_shape = (depth_stream.height(), depth_stream.width())
 
-            self.proc_params["rgb_2d_shape"] = self.rgb_2d_shape
-            self.proc_params["rgb_3d_shape"] = self.rgb_3d_shape
+            self.proc_params["bgr_2d_shape"] = self.bgr_2d_shape
+            self.proc_params["bgr_3d_shape"] = self.bgr_3d_shape
             self.proc_params["depth_2d_shape"] = self.depth_2d_shape
 
-            rgb_out_shape = self.rgb_3d_shape
+            bgr_out_shape = self.bgr_3d_shape
             depth_out_shape = self.depth_2d_shape
 
-            if align_depth_to_rgb:
-                depth_out_shape = self.rgb_2d_shape
+            if align_depth_to_bgr:
+                depth_out_shape = self.bgr_2d_shape
 
             pipeline.stop()
         else:
             dir_path = Path(directory_path)
 
             if not dir_path.is_dir():
-                raise ValueError(f"Directory {dir_path} does not exist.")
+                raise NotADirectoryError(f"{dir_path} is not a directory.")
 
-            if png_prefix is None or exr_prefix is None:
-                raise ValueError(f"<png_prefix> and <exr_prefix> have to be "
-                                 f"provided when <directory_path> is provided.")
-
-            rgb_sample_path = Path(directory_path) / f"{png_prefix}1.png"
+            bgr_sample_path = Path(directory_path) / f"{png_prefix}1.png"
             depth_sample_path = Path(directory_path) / f"{exr_prefix}1.exr"
 
-            rgb_sample_frame = cv2.imread(rgb_sample_path)
+            if not bgr_sample_path.is_file():
+                raise FileNotFoundError(f"{bgr_sample_path} is not a file.")
+            if not depth_sample_path.is_file():
+                raise FileNotFoundError(f"{depth_sample_path} is not a file.")
+
+            bgr_sample_frame = cv2.imread(str(bgr_sample_path))
             depth_sample_frame = \
-                cv2.imread(depth_sample_path,
+                cv2.imread(str(depth_sample_path),
                            cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
 
-            self.rgb_3d_shape = rgb_sample_frame.shape
-            self.rgb_2d_shape = self.rgb_3d_shape[:2]
-            self.depth_2d_shape = depth_sample_frame
+            self.bgr_3d_shape = bgr_sample_frame.shape
+            self.bgr_2d_shape = self.bgr_3d_shape[:2]
+            self.depth_2d_shape = depth_sample_frame.shape
 
-            self.proc_params["rgb_2d_shape"] = self.rgb_2d_shape
-            self.proc_params["rgb_3d_shape"] = self.rgb_3d_shape
+            self.proc_params["bgr_2d_shape"] = self.bgr_2d_shape
+            self.proc_params["bgr_3d_shape"] = self.bgr_3d_shape
             self.proc_params["depth_2d_shape"] = self.depth_2d_shape
 
-            if align_depth_to_rgb:
-                raise NotImplementedError("Aligning Depth frames to RGB "
+            if align_depth_to_bgr:
+                raise NotImplementedError("Aligning Depth frames to BGR "
                                           "frames is not implemented.")
 
-            rgb_out_shape = self.rgb_3d_shape
+            bgr_out_shape = self.bgr_3d_shape
             depth_out_shape = self.depth_2d_shape
 
-        self.rgb_out_port = OutPort(shape=rgb_out_shape)
+        self.bgr_out_port = OutPort(shape=bgr_out_shape)
         self.depth_out_port = OutPort(shape=depth_out_shape)
 
 
 @implements(proc=Realsense, protocol=LoihiProtocol)
 @requires(CPU)
 class LoihiDensePyRealsensePM(PyLoihiProcessModel):
-    rgb_out_port: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, int)
+    bgr_out_port: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, int)
     depth_out_port: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, int)
 
     def __init__(self, proc_params: dict) -> None:
         super().__init__(proc_params)
 
-        self._rgb_2d_shape = proc_params["rgb_2d_shape"]
-        self._rgb_3d_shape = proc_params["rgb_3d_shape"]
+        self._bgr_2d_shape = proc_params["bgr_2d_shape"]
+        self._bgr_3d_shape = proc_params["bgr_3d_shape"]
         self._depth_2d_shape = proc_params["depth_2d_shape"]
-        self._align_depth_to_rgb = proc_params["align_depth_to_rgb"]
+        self._align_depth_to_bgr = proc_params["align_depth_to_bgr"]
         self._directory_path = proc_params["directory_path"]
         self._png_prefix = proc_params["png_prefix"]
         self._exr_prefix = proc_params["exr_prefix"]
@@ -166,17 +168,15 @@ class LoihiDensePyRealsensePM(PyLoihiProcessModel):
 
             self._pipeline.start(config)
             self._align = rs.align(rs.stream.color)
-        else:
-            os.environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"
 
     def run_spk(self) -> None:
-        rgb_image, depth_image = self._get_frames()
+        bgr_image, depth_image = self._get_frames()
 
-        self.rgb_out_port.send(rgb_image)
+        self.bgr_out_port.send(bgr_image)
         self.depth_out_port.send(depth_image)
 
     def _get_frames(self) -> ty.Tuple[np.ndarray, np.ndarray]:
-        """Get RGB and Depth frames.
+        """Get BGR and Depth frames.
 
         If directory_path is None, get frames from camera.
         If directory_path is not None, get frames from files
@@ -184,8 +184,8 @@ class LoihiDensePyRealsensePM(PyLoihiProcessModel):
 
         Returns
         ----------
-        rgb_frame : np.ndarray
-            RGB frame.
+        bgr_frame : np.ndarray
+            BGR frame.
         depth_frame : np.ndarray
             Depth frame.
         """
@@ -195,12 +195,12 @@ class LoihiDensePyRealsensePM(PyLoihiProcessModel):
             return self._get_frames_from_directory()
 
     def _get_frames_from_camera(self) -> ty.Tuple[np.ndarray, np.ndarray]:
-        """Get RGB and Depth frames from camera.
+        """Get BGR and Depth frames from camera.
 
         Returns
         ----------
-        rgb_frame : np.ndarray
-            RGB frame.
+        bgr_frame : np.ndarray
+            BGR frame.
         depth_frame : np.ndarray
             Depth frame.
         """
@@ -208,31 +208,31 @@ class LoihiDensePyRealsensePM(PyLoihiProcessModel):
         if self._align_depth_to_rgb:
             frameset = self._align.process(frameset)
 
-        rgb_frame = np.array(frameset.get_color_frame().get_data())
+        bgr_frame = np.array(frameset.get_color_frame().get_data())
         depth_frame = np.array(frameset.get_depth_frame().get_data())
 
-        return rgb_frame, depth_frame
+        return bgr_frame, depth_frame
 
     def _get_frames_from_directory(self) -> ty.Tuple[np.ndarray, np.ndarray]:
-        """Get RGB and Depth frames from files.
+        """Get BGR and Depth frames from files.
 
         Returns
         ----------
-        rgb_frame : np.ndarray
-            RGB frame.
+        bgr_frame : np.ndarray
+            BGR frame.
         depth_frame : np.ndarray
             Depth frame.
         """
-        rgb_frame_path = Path(self._directory_path) / f"{self._png_prefix}" \
+        bgr_frame_path = Path(self._directory_path) / f"{self._png_prefix}" \
                                                       f"{self.time_step}.png"
         depth_frame_path = Path(self._directory_path) / f"{self._exr_prefix}" \
                                                         f"{self.time_step}.exr"
 
-        rgb_frame = cv2.imread(str(rgb_frame_path))
+        bgr_frame = cv2.imread(str(bgr_frame_path))
         depth_frame = cv2.imread(str(depth_frame_path),
                                  cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
 
-        return rgb_frame, depth_frame
+        return bgr_frame, depth_frame
 
     def _stop(self) -> None:
         if self._directory_path is None:
--- tests/lava/lib/peripherals/realsense/test_realsense.py
+++ tests/lava/lib/peripherals/realsense/test_realsense.py
@@ -2,10 +2,15 @@ import unittest
 import numpy as np
 import sys
 
+from lava.lib.peripherals.realsense.realsense import Realsense
+
+from lava.magma.core.run_configs import Loihi2HwCfg, Loihi2SimCfg
+from lava.magma.core.run_conditions import RunSteps, RunContinuous
+
+has_valid_camera = False
 try:
     import pyrealsense2 as rs
 
-    has_valid_camera = False
     for device in rs.context().devices:
         color_sensor = device.first_color_sensor()
         depth_sensor = device.first_depth_sensor()
@@ -17,44 +22,90 @@ except ImportError:
     print("Need `pyrealsense2` library installed.", file=sys.stderr)
     exit(1)
 
-from lava.magma.core.process.process import LogConfig
-
-from lava.magma.core.run_configs import Loihi2HwCfg, Loihi2SimCfg
-from lava.magma.core.run_conditions import RunSteps, RunContinuous
-
 
 class TestRealsense(unittest.TestCase):
     @unittest.skipUnless(has_valid_camera, "Requires valid Realsense camera.")
     def test_init_camera(self) -> None:
-        """"""
-        pass
+        """Test that the Realsense Process (reading from camera, aligning
+        Depth frames to BGR frames) is instantiated correctly."""
+        realsense = Realsense()
 
+        self.assertEqual(realsense.bgr_out_port.shape[:2],
+                         realsense.depth_out_port.shape)
+
+    @unittest.skipUnless(has_valid_camera, "Requires valid Realsense camera.")
+    def test_init_camera_not_align_depth_to_rgb(self) -> None:
+        """Test that the Realsense Process (reading from camera, not aligning
+        Depth frames to BGR frames) is instantiated correctly."""
+        realsense = Realsense(align_depth_to_bgr=False)
+
+        if realsense.bgr_2d_shape != realsense.depth_2d_shape:
+            self.assertNotEqual(realsense.bgr_out_port.shape[:2],
+                                realsense.depth_out_port.shape)
+
+    @unittest.skip("align_depth_to_rgb not implemented for files.")
     def test_init_files(self) -> None:
-        """"""
-        pass
+        """Test that the Realsense Process (reading from files, aligning
+        Depth frames to BGR frames) is instantiated correctly."""
+        realsense = Realsense(directory_path="recording",
+                              png_prefix="bgr_",
+                              exr_prefix="depth_")
+
+        self.assertEqual(realsense.bgr_out_port.shape, (480, 640, 3))
+        self.assertEqual(realsense.depth_out_port.shape, (480, 640))
+
+    def test_init_files_not_align_depth_to_rgb(self) -> None:
+        """Test that the Realsense Process (reading from files, not aligning
+        Depth frames to BGR frames) is instantiated correctly."""
+        realsense = Realsense(align_depth_to_bgr=False,
+                              directory_path="recording",
+                              png_prefix="bgr_",
+                              exr_prefix="depth_")
+
+        self.assertEqual(realsense.bgr_out_port.shape, (480, 640, 3))
+        self.assertEqual(realsense.depth_out_port.shape, (240, 320))
 
     def test_init_files_invalid_directory_path(self) -> None:
-        """"""
-        pass
+        """Test that initializing the Realsense Process with an invalid
+        directory_path raises an NotADirectoryError"""
+        with self.assertRaises(NotADirectoryError):
+            _ = Realsense(directory_path="invalid")
 
     def test_init_files_invalid_png_prefix(self) -> None:
-        """"""
-        pass
+        """Test that initializing the Realsense Process with an invalid
+        png_prefix raises a FileNotFoundError."""
+        with self.assertRaises(FileNotFoundError):
+            _ = Realsense(directory_path="recording",
+                          png_prefix="invalid",
+                          exr_prefix="depth_")
 
     def test_init_files_invalid_exr_prefix(self) -> None:
-        """"""
-        pass
+        """Test that initializing the Realsense Process with an invalid
+        exr_prefix raises a FileNotFoundError."""
+        with self.assertRaises(FileNotFoundError):
+            _ = Realsense(directory_path="recording",
+                          png_prefix="bgr_",
+                          exr_prefix="invalid")
 
 
 class TestLoihiDensePyRealsensePM(unittest.TestCase):
     @unittest.skipUnless(has_valid_camera, "Requires valid Realsense camera.")
     def test_run_camera(self) -> None:
-        """"""
-        pass
+        """Test that the Realsense Process (reading from camera) runs
+        correctly."""
+        realsense = Realsense()
+        realsense.run(condition=RunSteps(num_steps=2), run_cfg=Loihi2SimCfg())
+        realsense.stop()
 
     def test_run_files(self) -> None:
-        """"""
-        pass
+        """Test that the Realsense Process (reading from files) runs
+        correctly."""
+        realsense = Realsense(align_depth_to_bgr=False,
+                              directory_path="recording",
+                              png_prefix="bgr_",
+                              exr_prefix="depth_")
+        realsense.run(condition=RunSteps(num_steps=2), run_cfg=Loihi2SimCfg())
+        realsense.stop()
 
 
 if __name__ == "__main__":
