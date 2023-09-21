27cc99d1cf6a42b06e5df345df329be6ad47affc
shaline-koh <shaline.jia.thean.koh@intel.com>
2023-09-21 10:51:00 +0800

    Preliminary unit test for Prophesee camera

--- tests/lava/lib/peripherals/dvs/test_prophesee.py
+++ tests/lava/lib/peripherals/dvs/test_prophesee.py
@@ -31,15 +31,12 @@ from metavision_core.event_io import (
     RawReader, EventDatReader, EventsIterator)
 from metavision_sdk_cv import ActivityNoiseFilterAlgorithm
 
-USE_EVENTSITERATOR = True
-if USE_EVENTSITERATOR:
-    from lava.lib.peripherals.dvs.prophesee_eventsiterator import (
-        PropheseeCamera,
-        PyPropheseeCameraModel,
-        EventsIteratorWrapper)
-else:
-    from lava.lib.peripherals.dvs.prophesee_rawreader import \
-        PropheseeCamera, PyPropheseeCameraModel
+
+from lava.lib.peripherals.dvs.prophesee import (
+    PropheseeCamera,
+    PyPropheseeCameraRawReaderModel,
+    PyPropheseeCameraEventsIteratorModel,
+    EventsIteratorWrapper)
 
 
 SEQUENCE_FILENAME_RAW = "sparklers.raw"
@@ -60,15 +57,10 @@ except OSError:
 
 
 def get_shape(file_name):
-    if USE_EVENTSITERATOR:
-        mv_iterator = EventsIterator(input_path=file_name,
-                                     delta_t=1000)
-        height, width = mv_iterator.get_size()
-        del mv_iterator
-    else:
-        reader = RawReader(file_name)
-        height, width = reader.get_size()
-        del reader
+    mv_iterator = EventsIterator(input_path=file_name,
+                                    delta_t=1000)
+    height, width = mv_iterator.get_size()
+    del mv_iterator
     return height, width
 
 
@@ -159,9 +151,9 @@ class TestPropheseeCamera(unittest.TestCase):
             )
 
 
-class TestPyPropheseeCameraModel(unittest.TestCase):
+class TestPyPropheseeCameraModel_EventsIT(unittest.TestCase):
     def test_init(self):
-        """Test that the PyPropheseeCameraModel ProcessModel is instantiated
+        """Test that the PyPropheseeCameraEventsIteratorModel ProcessModel is instantiated
         correctly."""
 
         height, width = get_shape(SEQUENCE_FILENAME_RAW)
@@ -187,13 +179,11 @@ class TestPyPropheseeCameraModel(unittest.TestCase):
             "num_output_time_bins": num_output_time_bins,
         }
 
-        pm = PyPropheseeCameraModel(proc_params)
+        pm = PyPropheseeCameraEventsIteratorModel(proc_params)
 
-        self.assertIsInstance(pm, PyPropheseeCameraModel)
-        if USE_EVENTSITERATOR:
-            self.assertIsInstance(pm.reader, EventsIteratorWrapper)
-        else:
-            self.assertIsInstance(pm.reader, RawReader)
+        self.assertIsInstance(pm, PyPropheseeCameraEventsIteratorModel)
+
+        self.assertIsInstance(pm.reader, EventsIteratorWrapper)
 
     def test_base_functionality_file(self):
         """Test that running a PropheseeCamera works using a data file."""
@@ -207,7 +197,9 @@ class TestPyPropheseeCameraModel(unittest.TestCase):
         )
 
         run_condition = RunSteps(num_steps=num_steps)
-        run_cfg = Loihi2SimCfg()
+        custom_proc_model_map = {}
+        custom_proc_model_map[PropheseeCamera] = PyPropheseeCameraEventsIteratorModel
+        run_cfg = Loihi2SimCfg(exception_proc_model_map=custom_proc_model_map)
         camera.run(condition=run_condition, run_cfg=run_cfg)
         camera.stop()
 
@@ -224,7 +216,9 @@ class TestPyPropheseeCameraModel(unittest.TestCase):
         )
 
         run_condition = RunSteps(num_steps=num_steps)
-        run_cfg = Loihi2SimCfg()
+        custom_proc_model_map = {}
+        custom_proc_model_map[PropheseeCamera] = PyPropheseeCameraEventsIteratorModel
+        run_cfg = Loihi2SimCfg(exception_proc_model_map=custom_proc_model_map)
         camera.run(condition=run_condition, run_cfg=run_cfg)
         camera.stop()
 
@@ -236,7 +230,9 @@ class TestPyPropheseeCameraModel(unittest.TestCase):
         camera = PropheseeCamera(filename="", sensor_shape=(720, 1280))
 
         run_condition = RunSteps(num_steps=num_steps)
-        run_cfg = Loihi2SimCfg()
+        custom_proc_model_map = {}
+        custom_proc_model_map[PropheseeCamera] = PyPropheseeCameraEventsIteratorModel
+        run_cfg = Loihi2SimCfg(exception_proc_model_map=custom_proc_model_map)
         camera.run(condition=run_condition, run_cfg=run_cfg)
         camera.stop()
 
@@ -259,7 +255,9 @@ class TestPyPropheseeCameraModel(unittest.TestCase):
         )
 
         run_condition = RunSteps(num_steps=num_steps)
-        run_cfg = Loihi2SimCfg()
+        custom_proc_model_map = {}
+        custom_proc_model_map[PropheseeCamera] = PyPropheseeCameraEventsIteratorModel
+        run_cfg = Loihi2SimCfg(exception_proc_model_map=custom_proc_model_map)
         camera.run(condition=run_condition, run_cfg=run_cfg)
         camera.stop()
 
@@ -282,7 +280,9 @@ class TestPyPropheseeCameraModel(unittest.TestCase):
         )
 
         run_condition = RunSteps(num_steps=num_steps)
-        run_cfg = Loihi2SimCfg()
+        custom_proc_model_map = {}
+        custom_proc_model_map[PropheseeCamera] = PyPropheseeCameraEventsIteratorModel
+        run_cfg = Loihi2SimCfg(exception_proc_model_map=custom_proc_model_map)
         camera.run(condition=run_condition, run_cfg=run_cfg)
         camera.stop()
 
@@ -304,7 +304,9 @@ class TestPyPropheseeCameraModel(unittest.TestCase):
         )
 
         run_condition = RunSteps(num_steps=num_steps)
-        run_cfg = Loihi2SimCfg()
+        custom_proc_model_map = {}
+        custom_proc_model_map[PropheseeCamera] = PyPropheseeCameraEventsIteratorModel
+        run_cfg = Loihi2SimCfg(exception_proc_model_map=custom_proc_model_map)
         camera.run(condition=run_condition, run_cfg=run_cfg)
         camera.stop()
 
@@ -332,7 +334,9 @@ class TestPyPropheseeCameraModel(unittest.TestCase):
         camera.s_out.connect(recv.in_port)
 
         run_condition = RunSteps(num_steps=num_steps)
-        run_cfg = Loihi2SimCfg()
+        custom_proc_model_map = {}
+        custom_proc_model_map[PropheseeCamera] = PyPropheseeCameraEventsIteratorModel
+        run_cfg = Loihi2SimCfg(exception_proc_model_map=custom_proc_model_map)
         camera.run(condition=run_condition, run_cfg=run_cfg)
         recv_data = recv.buffer.get()
         camera.stop()
@@ -350,7 +354,9 @@ class TestPyPropheseeCameraModel(unittest.TestCase):
         )
 
         run_condition = RunContinuous()
-        run_cfg = Loihi2SimCfg()
+        custom_proc_model_map = {}
+        custom_proc_model_map[PropheseeCamera] = PyPropheseeCameraEventsIteratorModel
+        run_cfg = Loihi2SimCfg(exception_proc_model_map=custom_proc_model_map)
         camera.run(condition=run_condition, run_cfg=run_cfg)
         time.sleep(0.1)
         camera.pause()
@@ -358,3 +364,218 @@ class TestPyPropheseeCameraModel(unittest.TestCase):
         camera.run(condition=run_condition, run_cfg=run_cfg)
         time.sleep(0.1)
         camera.stop()
+
+class TestPyPropheseeCameraModel_RawReader(unittest.TestCase):
+    def test_init(self):
+        """Test that the PyPropheseeCameraRawReaderModel ProcessModel is instantiated
+        correctly."""
+
+        height, width = get_shape(SEQUENCE_FILENAME_RAW)
+
+        transformations = Compose(
+            [
+                Downsample(factor=0.5),
+            ]
+        )
+        num_output_time_bins = 2
+
+        proc_params = {
+            "shape": (num_output_time_bins, 2, height, width),
+            "filename": SEQUENCE_FILENAME_RAW,
+            "biases": None,
+            "filters": [
+                ActivityNoiseFilterAlgorithm(
+                    width=width, height=height, threshold=1000
+                )
+            ],
+            "max_events_per_dt": 10**8,
+            "transformations": transformations,
+            "num_output_time_bins": num_output_time_bins,
+        }
+
+        pm = PyPropheseeCameraRawReaderModel(proc_params)
+
+        self.assertIsInstance(pm, PyPropheseeCameraRawReaderModel)
+    
+        self.assertIsInstance(pm.reader, RawReader)
+
+    def test_base_functionality_file(self):
+        """Test that running a PropheseeCamera works using a data file."""
+
+        height, width = get_shape(SEQUENCE_FILENAME_RAW)
+
+        num_steps = 2
+
+        camera = PropheseeCamera(
+            filename=SEQUENCE_FILENAME_RAW, sensor_shape=(height, width)
+        )
+
+        run_condition = RunSteps(num_steps=num_steps)
+        custom_proc_model_map = {}
+        custom_proc_model_map[PropheseeCamera] = PyPropheseeCameraRawReaderModel
+        run_cfg = Loihi2SimCfg(exception_proc_model_map=custom_proc_model_map)
+        camera.run(condition=run_condition, run_cfg=run_cfg)
+        camera.stop()
+
+    def test_base_functionality_dat_file(self):
+        """Test that running a PropheseeCamera works
+        using a dat data file."""
+        # The DAT file should have the same resolution as the RAW file
+
+        height, width = get_shape(SEQUENCE_FILENAME_RAW)
+        num_steps = 2
+
+        camera = PropheseeCamera(
+            filename=SEQUENCE_FILENAME_DAT, sensor_shape=(height, width)
+        )
+
+        run_condition = RunSteps(num_steps=num_steps)
+        custom_proc_model_map = {}
+        custom_proc_model_map[PropheseeCamera] = PyPropheseeCameraRawReaderModel
+        run_cfg = Loihi2SimCfg(exception_proc_model_map=custom_proc_model_map)
+        camera.run(condition=run_condition, run_cfg=run_cfg)
+        camera.stop()
+
+    @unittest.skipUnless(USE_CAMERA_TESTS, "Needs live camera")
+    def test_base_functionality_camera(self):
+        """Test that running a PropheseeCamera works using a camera."""
+        num_steps = 2
+
+        camera = PropheseeCamera(filename="", sensor_shape=(720, 1280))
+
+        run_condition = RunSteps(num_steps=num_steps)
+        custom_proc_model_map = {}
+        custom_proc_model_map[PropheseeCamera] = PyPropheseeCameraRawReaderModel
+        run_cfg = Loihi2SimCfg(exception_proc_model_map=custom_proc_model_map)
+        camera.run(condition=run_condition, run_cfg=run_cfg)
+        camera.stop()
+
+    @unittest.skipUnless(USE_CAMERA_TESTS, "Needs live camera")
+    def test_biases(self):
+        """Test that setting biases works"""
+
+        num_steps = 2
+        biases = {
+            "bias_diff": 80,
+            "bias_diff_off": 25,
+            "bias_diff_on": 140,
+            "bias_fo": 74,
+            "bias_hpf": 0,
+            "bias_refr": 68,
+        }
+
+        camera = PropheseeCamera(
+            filename="", biases=biases, sensor_shape=(720, 1280)
+        )
+
+        run_condition = RunSteps(num_steps=num_steps)
+        custom_proc_model_map = {}
+        custom_proc_model_map[PropheseeCamera] = PyPropheseeCameraRawReaderModel
+        run_cfg = Loihi2SimCfg(exception_proc_model_map=custom_proc_model_map)
+        camera.run(condition=run_condition, run_cfg=run_cfg)
+        camera.stop()
+
+    def test_filters(self):
+        """Test that setting biases works"""
+
+        height, width = get_shape(SEQUENCE_FILENAME_RAW)
+
+        num_steps = 2
+        filters = [
+            ActivityNoiseFilterAlgorithm(
+                width=width, height=height, threshold=1000
+            )
+        ]
+
+        camera = PropheseeCamera(
+            filename=SEQUENCE_FILENAME_RAW,
+            sensor_shape=(height, width),
+            filters=filters,
+        )
+
+        run_condition = RunSteps(num_steps=num_steps)
+        custom_proc_model_map = {}
+        custom_proc_model_map[PropheseeCamera] = PyPropheseeCameraRawReaderModel
+        run_cfg = Loihi2SimCfg(exception_proc_model_map=custom_proc_model_map)
+        camera.run(condition=run_condition, run_cfg=run_cfg)
+        camera.stop()
+
+    def test_transformations(self):
+        """Test that setting biases works"""
+
+        height, width = get_shape(SEQUENCE_FILENAME_RAW)
+
+        num_steps = 2
+        transformations = Compose(
+            [
+                Downsample(factor=0.5),
+            ]
+        )
+        camera = PropheseeCamera(
+            filename=SEQUENCE_FILENAME_RAW,
+            sensor_shape=(height, width),
+            transformations=transformations,
+        )
+
+        run_condition = RunSteps(num_steps=num_steps)
+        custom_proc_model_map = {}
+        custom_proc_model_map[PropheseeCamera] = PyPropheseeCameraRawReaderModel
+        run_cfg = Loihi2SimCfg(exception_proc_model_map=custom_proc_model_map)
+        camera.run(condition=run_condition, run_cfg=run_cfg)
+        camera.stop()
+
+    def test_data_received(self):
+        """Test that the PropheseeCamera process correctly reads and sends
+        data."""
+
+        height, width = get_shape(SEQUENCE_FILENAME_RAW)
+        num_steps = 1
+
+        transformations = Compose(
+            [
+                Downsample(factor=0.1),
+            ]
+        )
+
+        camera = PropheseeCamera(
+            filename=SEQUENCE_FILENAME_RAW,
+            sensor_shape=(height, width),
+            transformations=transformations,
+        )
+
+        recv = Recv(shape=camera.s_out.shape, buffer_size=num_steps)
+
+        camera.s_out.connect(recv.in_port)
+
+        run_condition = RunSteps(num_steps=num_steps)
+        custom_proc_model_map = {}
+        custom_proc_model_map[PropheseeCamera] = PyPropheseeCameraRawReaderModel
+        run_cfg = Loihi2SimCfg(exception_proc_model_map=custom_proc_model_map)
+        camera.run(condition=run_condition, run_cfg=run_cfg)
+        recv_data = recv.buffer.get()
+        camera.stop()
+
+        self.assertTrue(np.any(recv_data > 0))
+
+    def test_pause(self):
+        """Test pausing the network does not cause any harm.
+        Data will get droped for the pause duration."""
+
+        height, width = get_shape(SEQUENCE_FILENAME_RAW)
+
+        camera = PropheseeCamera(
+            filename=SEQUENCE_FILENAME_RAW, sensor_shape=(height, width)
+        )
+
+        run_condition = RunContinuous()
+        custom_proc_model_map = {}
+        custom_proc_model_map[PropheseeCamera] = PyPropheseeCameraRawReaderModel
+        run_cfg = Loihi2SimCfg(exception_proc_model_map=custom_proc_model_map)
+        camera.run(condition=run_condition, run_cfg=run_cfg)
+        time.sleep(0.1)
+        camera.pause()
+        time.sleep(0.1)
+        camera.run(condition=run_condition, run_cfg=run_cfg)
+        time.sleep(0.1)
+        camera.stop()
+        
